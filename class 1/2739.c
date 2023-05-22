3f87ef52c845a87d7ec52b20a1575b92853e37d0
hoeyoon <89262375+hoeyoon@users.noreply.github.com>
2023-05-22 13:42:09 +0900

    구구단

---
+++ class 1/2739.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  int x;
+  scanf("%d", &x);
+  for(int i = 1; i <= 9; i++){
+    int sum = x * i;
+    printf("%d * %d = %d\n", x, i, sum); 
+  }
+  return 0;
+}
